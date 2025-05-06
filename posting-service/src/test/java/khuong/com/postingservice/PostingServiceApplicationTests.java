7b748acc3e6f38b07edcf0ea399db19e867f7156
khuong2924 <truongkhuong666@gmail.com>
2025-05-06 20:05:10 +0700

    init posting domain

---
+++ posting-service/src/test/java/khuong/com/postingservice/PostingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package khuong.com.postingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
